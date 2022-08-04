3a011026d8d9e0024f730ce6e5cd4d7ca962336a
sandaru <sandaruabayamanna@gmail.com>
2022-08-04 22:10:59 +0530

    Login frontend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const app = express();
 const cors=require("cors");
 const connection=require("./db");
 
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+
 //DB connection
 connection();
 
@@ -11,7 +14,9 @@ connection();
 app.use(express.json())
 app.use(cors());
 
+//routes
+app.use("/api/users",userRoutes);
+app.use("/api.auth",authRoutes);
+
 const port=process.env.PORT||8080;
 app.listen(port,()=>console.log(`Listening on port ${port}...`));
-
-
